445eb8750b6b6f237027154b16ea72b471b2718a
towasme <towasm@gmail.com>
2020-06-05 18:22:55 -0500

    adding home

---
+++ screens/homoe/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {navigation} from '@react-navigation/native';
+import { View, Image, Button} from 'react-native';
+import styles from './styles';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image source={require('../../assets/apadrinapp_logo.png')}
+          style={styles.logo}/>
+          <Button title='Next' onPress={() => navigation.navigate('Login')} />
+      </View>
+    );
+  }
+}
+
+export default Home;
---
+++ screens/homoe/styles.js
@@ -0,0 +1,16 @@
+import Color from '../../source/utils/Colors';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: Color.purple,
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      logo: {
+        height: 200,
+        width: 160,
+      }
+});
+
+export default styles;
