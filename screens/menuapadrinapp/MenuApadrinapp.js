8ae0c647d28de5b776559c00a319885c428027a9
andres0191 <andres_felipe0191@hotmail.com>
2020-06-08 08:41:44 -0500

    correction error of the file MenuApadrinapp.js when reazed git pull of the branch master

--- screens/menuapadrinapp/MenuApadrinapp.js
+++ screens/menuapadrinapp/MenuApadrinapp.js
@@ -6,7 +6,7 @@ import firebaseService from '../../services/firebase';
 const MenuApadrinapp = ({ navigation }) => {
   return (
    <View style={styles.container}>
-     <Image source={require('../../assets/apadrinapp_logo.png')} style={styles.logo}
+     <Image source={require('../../assets/apadrina.png')} style={styles.logo}
         />
      <TouchableHighlight style={[styles.button, styles.next]} onPress={() => navigation.navigate('PublicacionesRappi')}>
          <Text style={styles.textButton} >Lista de ahijados</Text>
