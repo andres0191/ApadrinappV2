07e1be22543d467a1b31d04f5cde380d5bed1e9b
towasme <towasm@gmail.com>
2020-06-13 22:24:39 -0500

    login saves the email and passowrd, and erases the info with a button

--- screens/login/login.js
+++ screens/login/login.js
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Text, View, TouchableHighlight, TextInput, Image, AsyncStorage } from 'react-native';
 import styles from './styles';
 import firebaseService from '../../services/firebase';
 import YellowBigButton from '../../source/Components/YellowBigButton';
 import WhiteBigButton from '../../source/Components/WhiteBigButton';
 
-let USEREMAIL = 'sinUser';
+let USEREMAIL = 'sinUser'
+let PASSW = "NoPass"
 const login = ({navigation}) => {
     const [user, setUser] = useState('')
     const [pass, setPass] = useState('')
@@ -14,12 +15,47 @@ const login = ({navigation}) => {
         try{
             await firebaseService.login(user, pass)
             await AsyncStorage.setItem(USEREMAIL, user);
+            await AsyncStorage.setItem(PASSW, pass);
             navigation.navigate('MenuApadrinapp')
         } catch (e) {
             alert('Por favor ingres los datos correctos')
-            
         }
     }
+
+    const load = async () => {
+        try {
+            let user = await AsyncStorage.getItem(USEREMAIL)
+            let pass = await AsyncStorage.getItem(PASSW)
+
+            if (USEREMAIL !== null) {
+                setUser(user);
+            }
+            if (PASSW !== null) {
+                setPass(pass);
+            }
+        } catch (e) {
+            alert(err);
+        }
+    };
+
+    const remove = async () => {
+        try {
+            await AsyncStorage.removeItem(USEREMAIL);
+            await AsyncStorage.removeItem(PASSW);
+        } catch (err) {
+            alert(err);
+        }
+
+        finally {
+            setUser("");
+            setPass("");
+        }
+    }
+
+    useEffect(() => {
+        load()
+    }, []);
+
 
   return (
     <View style={styles.container}>
@@ -32,17 +68,20 @@ const login = ({navigation}) => {
                 placeholderTextColor="white"
                 style={styles.inputText}
                 value = {user}
-                onChange = {(e) => setUser(e.nativeEvent.text)}/>
+                onChange = {(e) => setUser(e.nativeEvent.text)}
+                onChangeText={(text) => setUser(text)}/>
             <TextInput
-                placeholder='Ingresa tu contrasena'
+                placeholder='Ingresa tu contraseña'
                 placeholderTextColor="white"
                 style={styles.inputText}
                 value = {pass}
-                onChange = {(e) => setPass(e.nativeEvent.text)}/>
+                onChange = {(e) => setPass(e.nativeEvent.text)}
+                onChangeText={(text) => setPass(text)}/>
         </View>
         <View style={styles.MenuOptions}>
             <WhiteBigButton title='Ingresa' onPress={onPressLogin}></WhiteBigButton>
-            <YellowBigButton title='Registrate' onPress={() => navigation.navigate('SignUp')}></YellowBigButton>   
+            <WhiteBigButton title='borrar' onPress={() => remove()}></WhiteBigButton>
+            <YellowBigButton title='Registrate' onPress={() => navigation.navigate('SignUp')}></YellowBigButton>
         </View>
     </View>
   );
