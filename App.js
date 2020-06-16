894633dd278159691708ed0cb05a4938cdb6ab79
andres0191 <andres_felipe0191@hotmail.com>
2020-06-16 17:59:42 -0500

    update app.js

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { createStackNavigator} from '@react-navigation/stack';
 import Login from './screens/login/login';
 import SignUp from './screens/signUp/SignUp';
-/* import Inversion from './screens/inversion/inversion'; */
 import Start from './screens/start/start';
 import MenuApadrinapp from './screens/menuapadrinapp/MenuApadrinapp';
 import PublicacionesRappi from './screens/publicacionesrappi/PublicacionesRappi';
@@ -12,20 +11,21 @@ import EstadoCuenta from './screens/estadocuenta/Estadocuenta';
 import Ciclo from './screens/slides/Ciclo';
 import Dreamer from './screens/slides/Dreamer';
 import Padrino from './screens/slides/Padrino';
-import Color from './source/utils/Colors';
 import List from './screens/publicacionesrappi/List';
 import NameLogin from './screens/NameLogin/NameLogin';
-import { startClock } from 'react-native-reanimated';
 import SaveFunction from './screens/PruebasAndres/SaveFunction';
 import PostRappi from './screens/Rappitenderos/PostRappi';
+import {decode, encode} from 'base-64'
 
+if (!global.btoa) {  global.btoa = encode }
+if (!global.atob) { global.atob = decode }
 const Stack = createStackNavigator()
 
 export default function App(){
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen options={{ headerShown : false }} name='Start' component={SaveFunction} />
+        <Stack.Screen options={{ headerShown : false }} name='Start' component={Start} />
         <Stack.Screen options={{ headerShown : false }} name='SignUp' component={SignUp} />
         <Stack.Screen options={{ headerShown : false }} name='Login' component={Login} />
         <Stack.Screen options={{ headerShown : false }} name='MenuApadrinapp' component={MenuApadrinapp} />
