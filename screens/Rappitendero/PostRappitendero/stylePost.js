c8c94e01039b34527ba5dcd6099accf7caebe337
andres0191 <andres_felipe0191@hotmail.com>
2020-07-03 17:26:40 -0500

    update singleRappiPost and stylePost

--- screens/Rappitendero/PostRappitendero/stylePost.js
+++ screens/Rappitendero/PostRappitendero/stylePost.js
@@ -1,25 +1,26 @@
+import React from 'react';
 import { StyleSheet } from 'react-native';
 import Color from '../../../source/utils/Colors';
 import { Dimensions } from 'react-native';
 
 const win = Dimensions.get('window');
-const ratio = win.width/550;
-const styles = StyleSheet.create({
+const ratio = win.width/1100;
 
+const styles = StyleSheet.create({
   container : {
     flex: 1,
     flexDirection : 'column',
     justifyContent: 'center',
     alignContent: 'center',
-    backgroundColor : Color.white,
+    backgroundColor : Color.grey,
   },
   header : {
     flex : 1,
     paddingTop: 22,
-    // marginBottom: -90,
     backgroundColor: Color.lightpurple
   },
   headerLeft : {
+    flex : 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'baseline',
@@ -27,55 +28,98 @@ const styles = StyleSheet.create({
     paddingBottom: 12,
     backgroundColor: Color.lightpurple
   },
-  // headerMiddle : {
-  // },
-  // headerRight : {
-  // },
-  body : {
-    flex : 1.5,
-    alignItems : 'center',
-    justifyContent : 'center',
-    backgroundColor: Color.lightpurple
+  headerMiddle : {
+    flex: 1,
   },
-  logo : {
-    width: win.width,
-    height: 450 * ratio,
-    justifyContent: 'center',
-  },
-  initialText: {
-    fontSize: 20,
-    paddingVertical: 10,
-    paddingHorizontal: 30
-  },
-  inputText: {
-    height: 40,
-    minWidth : 180,
-    borderWidth: 1,
-    borderColor: '#ccc',
-    paddingHorizontal: 15,
-    color: Color.purple,
-    backgroundColor: Color.lightpurple,
-    borderRadius: 10,
-    marginTop: 10,
-    fontSize: 20,
-  },
-  footer: {
-    flex: 0.1,
-    flexDirection : 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: Color.purple
+  headerRight : {
+    flex: 1,
   },
   PageTitle: {
     fontFamily: '',
     fontSize: 18,
     color: Color.purple,
   },
-  ElevatePic: {
-    elevation: 2, 
+  body : {
+    flex : 10,
+    flexDirection: 'column',
+    alignItems : 'center',
+    justifyContent : 'center',
+    backgroundColor: Color.lightpurple,
   },
-  buttons : {
-    justifyContent : 'space-evenly'
+  logo : {
+    width: win.width,
+    height: 450 * ratio,
+    justifyContent: 'center',
+  },
+  ElevatePic: {
+    elevation: 2,
+    marginBottom: -10
+  },
+  InfoCube: {
+    flexDirection : 'row',
+    width: win.width,
+    justifyContent: 'center',
+  },
+  info: {
+    width: win.width,
+    flexDirection : 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  footer: {
+    flex: 1,
+    flexDirection : 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: Color.purple,
+    position: 'absolute',
+    bottom: 0,
+    width: win.width,
+  },
+  InputInfo: {
+    flex: .5,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 10
+  },
+  YellowFont: {
+    /*fontFamily: '',*/
+    marginBottom: -8,
+    fontSize: 17,
+    fontWeight: "bold",
+    color: Color.yellow,
+  },
+  AllBoxes: {
+    backgroundColor: Color.lightpurple
+  },
+  Boxes: {
+    padding: 30,
+    backgroundColor: Color.purple,
+    width: win.width/1.05,
+    margin: 5,
+    borderColor: Color.lightpurple,
+    borderRadius: 20
+  },
+  Dreamer: {
+    color: Color.yellow,
+    fontSize: 20,
+    fontWeight: "bold"
+  },
+  ItemName: {
+    fontSize: 30,
+    color: Color.white,
+    fontWeight: "bold"
+  },
+  ItemMonto: {
+    fontSize: 18,
+    color: Color.grey,
+    fontWeight: "bold"
+  },
+  ItemDescription: {
+    color: Color.grey,
+    fontSize: 24,
+    marginTop: 10
   }
 })
 
