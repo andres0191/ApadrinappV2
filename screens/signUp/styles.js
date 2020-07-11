477298bb6bee8bc9610027ca9b0c13d8332fd74a
towasme <towasm@gmail.com>
2020-07-10 23:00:12 -0500

    added hide password for sign uo

--- screens/signUp/styles.js
+++ screens/signUp/styles.js
@@ -25,6 +25,30 @@ const styles = StyleSheet.create({
         marginTop: 10,
         fontSize: 20,
       },
+      PassIn: {
+        minHeight: 50,
+        maxHeight: 50,
+        minWidth: 250,
+        maxWidth: 250,
+        borderWidth: 1,
+        borderColor: Color.yellow,
+        paddingHorizontal: 15,
+        backgroundColor: Color.lightpurple,
+        borderRadius: 10,
+        marginTop: 10,
+        flexDirection: "row",
+        alignItems: 'center',
+        justifyContent: "space-between",
+      },
+      PassText: {
+        fontSize: 20,
+        color: Color.purple,
+      },
+      Eye: {
+        height: 25,
+        width: 40,
+        marginLeft: -122
+      },
       logo: {
         height: 200,
         width: 160,
